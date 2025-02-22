f895c78d8af9b06d1ea12937c47fd96f14800398
Bartłomiej Rudecki <przekichane@icloud.com>
2025-02-22 13:19:49 +0100

    fix(lib/server/db): do not check if env.DATABASE_URL exists while building

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,11 @@
 import * as schema from '$lib/server/db/schema';
 
+import { building } from '$app/environment';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) {
+if (!building && !env.DATABASE_URL) {
 	throw new Error('DATABASE_URL is not set');
 }
 
