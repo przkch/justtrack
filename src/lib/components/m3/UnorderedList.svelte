55e9672635ef7400aabf0da7a2cca3f680daf675
Bartłomiej Rudecki <przekichane@icloud.com>
2025-02-23 18:01:31 +0100

    feat(components/m3): init bare ul/li components
    * to be improved in the future, i guess

---
+++ src/lib/components/m3/UnorderedList.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<ul class="list-inside list-disc">
+	{@render children?.()}
+</ul>
---
+++ src/lib/components/m3/index.ts
@@ -0,0 +1,2 @@
+export { default as ListItem } from '$lib/components/m3/ListItem.svelte';
+export { default as UnorderedList } from '$lib/components/m3/UnorderedList.svelte';
