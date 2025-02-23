def4ffc1957e694ab1e82bea035a290701f10335
Bartłomiej Rudecki <przekichane@icloud.com>
2025-02-23 13:25:39 +0100

    chore: move Navbar to a separate component

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { Button } from 'm3-svelte';
+</script>
+
+<nav
+	class="m3-container sticky top-4 mx-auto flex max-w-[900px] flex-row items-center justify-between gap-2 rounded-xl p-2"
+>
+	<a href="/" class="text-xl">JustTrack</a>
+	<div class="flex flex-row items-center gap-2">
+		{#if page.data.session}
+			{#if page.data.session.user?.image}
+				<img src={page.data.session.user.image} class="w-8 rounded-full" alt="User Avatar" />
+			{/if}
+			<Button type="filled" on:click={() => signOut()}>Sign out</Button>
+		{:else}
+			<Button type="filled" on:click={() => signIn('github')}>Sign in</Button>
+		{/if}
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import M3Style from '$lib/components/M3Style.svelte';
-
-	import { page } from '$app/state';
-	import { signIn, signOut } from '@auth/sveltekit/client';
-	import { Button } from 'm3-svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
@@ -15,19 +12,7 @@
 
 <M3Style />
 
-<nav class="flex flex-row items-center justify-between gap-2 p-4">
-	<a href="/" class="text-xl">JustTrack</a>
-	<div class="flex flex-row items-center gap-2">
-		{#if page.data.session}
-			{#if page.data.session.user?.image}
-				<img src={page.data.session.user.image} class="w-8 rounded-full" alt="User Avatar" />
-			{/if}
-			<Button type="filled" on:click={() => signOut()}>Sign out</Button>
-		{:else}
-			<Button type="filled" on:click={() => signIn('github')}>Sign in</Button>
-		{/if}
-	</div>
-</nav>
+<Navbar />
 
 <main class="mx-auto max-w-[900px] p-4">
 	{@render children()}
