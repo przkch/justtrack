17e72bc87c62f54d1354c66f595610a54fbaff8d
Bartłomiej Rudecki <przekichane@icloud.com>
2025-02-23 13:58:43 +0100

    feat: add unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import Icons from 'unplugin-icons/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [basicSsl(), sveltekit(), tailwindcss()]
+	plugins: [
+		basicSsl(),
+		Icons({
+			compiler: 'svelte'
+		}),
+		sveltekit(),
+		tailwindcss()
+	]
 });
