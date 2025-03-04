d6bff3b4a9ec48cb7eaad5aca4538a48956fd552
Bartłomiej Rudecki <przekichane@icloud.com>
2025-03-04 22:53:05 +0100

    feat(tv): add seasons list

--- src/routes/tv/[tvId]/+page.svelte
+++ src/routes/tv/[tvId]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Container from '$lib/components/media/Container.svelte';
+	import Poster from '$lib/components/media/Poster.svelte';
 
 	import type { PageProps } from './$types';
+	import { Chip } from 'm3-svelte';
 
 	let { data }: PageProps = $props();
 	const tv = data.tv;
@@ -11,4 +13,50 @@
 	<title>{tv.name} | JustTrack</title>
 </svelte:head>
 
-<Container {tv} />
+<div class="flex flex-col gap-8">
+	<Container {tv} />
+
+	<div class="flex flex-col gap-8">
+		{#if !tv.seasons}
+			<p>this show has no seasons...</p>
+		{:else}
+			<h3 class="text-2xl font-bold">
+				{#if tv.seasons.length === 1}
+					1 season
+				{:else}
+					{tv.seasons!.length} seasons
+				{/if}
+			</h3>
+
+			<div class="flex flex-col gap-4 not-last:border-b">
+				{#each tv.seasons as season, index}
+					<div class="flex flex-row items-center gap-4 rounded-xl">
+						<Poster posterPath={season.poster_path} size="sm" />
+
+						<div class="flex flex-col justify-center gap-4">
+							<span class="text-xl">
+								<a href={`/tv/${tv.tvId}/season/${index + 1}`}>
+									{season.name}
+								</a>
+							</span>
+
+							<div class="flex flex-row flex-wrap gap-2">
+								<Chip type="general">
+									{new Date(season.air_date).toLocaleDateString('en', {
+										year: 'numeric',
+										month: 'long',
+										day: 'numeric'
+									})}
+								</Chip>
+								<Chip type="general">
+									{Math.round(Number(season.vote_average) * 10)}% liked
+								</Chip>
+								<Chip type="general">{season.episode_count} episodes</Chip>
+							</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+		{/if}
+	</div>
+</div>
