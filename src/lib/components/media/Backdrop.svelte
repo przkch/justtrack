386ac1824fd22796f2f1bd5a0e050d79cb2ad247
Bartłomiej Rudecki <przekichane@icloud.com>
2025-03-19 17:48:44 +0100

    fix(components/media/Backdrop): make the backdrop invisible on light mode

--- src/lib/components/media/Backdrop.svelte
+++ src/lib/components/media/Backdrop.svelte
@@ -10,18 +10,14 @@
 
 {#if backdropPath}
 	<div
-		class="fixed top-0 right-0 left-0 -z-[1000] flex h-screen w-screen flex-col overflow-hidden brightness-[0.3]"
+		class="invisible fixed top-0 right-0 left-0 -z-[1000] flex h-screen w-screen flex-col overflow-hidden brightness-[0.3] dark:visible"
 	>
 		<img
 			src={`https://image.tmdb.org/t/p/original/${backdropPath}`}
-			class={[
-				'aspect-[2/3] h-screen max-w-screen object-cover object-center sm:aspect-[2/1]',
-				className
-			]}
+			class={['aspect-[2/3] h-screen max-w-screen object-cover object-center', className]}
 			width={1920}
 			height={540}
 			decoding="async"
-			style="background: radial-gradient(70% 110% at 50% 0%, rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 100%);"
 			{...rest}
 		/>
 	</div>
